dc84bfab6abff889fc6fb089dd64f76c1f1129f6
Novixous <anonymousvhb@gmail.com>
2021-09-26 00:30:31 +0700

    push code

--- 05-starting-setup/src/App.js
+++ 05-starting-setup/src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './App.css';
+import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
+import Button from "./components/UI/Button/Button";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((previous) => !previous);
+  };
+
+  console.log("APP RUNNING");
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={false} />
+      {/* {showParagraph && <p>This is new</p>} */}
+      <Button onClick={toggleParagraphHandler}>Show Paragraph</Button>
     </div>
   );
 }
