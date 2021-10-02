6ae47fa06bb6b6d150cdd160f2b3dc30513b4db6
Novixous <anonymousvhb@gmail.com>
2021-10-02 22:30:13 +0700

    done 242

--- 13-redux-first-learning-app/src/components/Counter.js
+++ 13-redux-first-learning-app/src/components/Counter.js
@@ -1,5 +1,6 @@
 import classes from "./Counter.module.css";
 import { useSelector, useDispatch } from "react-redux";
+import { counterActions } from "../store/index";
 // import { connect } from "react-redux";
 // import { Component } from "react";
 
@@ -9,19 +10,19 @@ const Counter = () => {
   const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
 
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
 
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 5 });
+    dispatch(counterActions.increase(5));
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
