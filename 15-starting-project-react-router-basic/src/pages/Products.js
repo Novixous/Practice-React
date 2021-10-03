d6e7714337cadd579eac146372fd037b964532f3
Novixous <anonymousvhb@gmail.com>
2021-10-03 20:59:58 +0700

    done 270

--- 15-starting-project-react-router-basic/src/pages/Products.js
+++ 15-starting-project-react-router-basic/src/pages/Products.js
@@ -2,9 +2,15 @@ import React from "react";
 
 const Products = () => {
   return (
-    <div>
-      <h1>The Product Page</h1>
-    </div>
+    <section>
+      <h1>The Products Page</h1>
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+        <li>Product 4</li>
+      </ul>
+    </section>
   );
 };
 
