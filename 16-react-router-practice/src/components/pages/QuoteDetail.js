504a422df1aa00e44b7e73e7c640e30d5bc68cab
Novixous <anonymousvhb@gmail.com>
2021-10-03 22:26:02 +0700

    done 277

--- 16-react-router-practice/src/components/pages/QuoteDetail.js
+++ 16-react-router-practice/src/components/pages/QuoteDetail.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Fragment } from "react";
+import { Route, useParams } from "react-router";
+import Comments from "../comments/Comments";
 
 const QuoteDetail = () => {
-  return <h1>Quote Detail Page</h1>;
+  const params = useParams();
+  return (
+    <Fragment>
+      <h1>Quote Detail Page</h1>
+      <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
 };
 
 export default QuoteDetail;
