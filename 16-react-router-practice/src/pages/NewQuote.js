72850f9b1f70a3e36abbff56813b4b6943521f5b
Novixous <anonymousvhb@gmail.com>
2021-10-04 09:47:10 +0700

    done 287

--- 16-react-router-practice/src/pages/NewQuote.js
+++ 16-react-router-practice/src/pages/NewQuote.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useHistory } from "react-router";
 import QuoteForm from "../components/quotes/QuoteForm";
+import { addQuote } from "../lib/api";
+import useHttp from "../hooks/use-http";
 
 const NewQuote = () => {
+  const { sendRequest, status } = useHttp(addQuote);
   const history = useHistory();
 
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
+    }
+  }, [status, history]);
+
   const addQuoteHandler = (quoteData) => {
-    history.push("/quotes");
+    sendRequest(quoteData);
   };
-  return <QuoteForm onAddQuote={addQuoteHandler} />;
+  return (
+    <QuoteForm isLoading={status === "pending"} onAddQuote={addQuoteHandler} />
+  );
 };
 
 export default NewQuote;
