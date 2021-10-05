edd97406cfe5e57a9287706ebe348f2efed8ec2c
Novixous <anonymousvhb@gmail.com>
2021-10-05 17:32:38 +0700

    done 339

--- 18-nextjs-onwards-to-a-bigger-project-starting-project/pages/[meetupId]/index.js
+++ 18-nextjs-onwards-to-a-bigger-project-starting-project/pages/[meetupId]/index.js
@@ -1,14 +1,48 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-const MeetupDetails = () => {
+const MeetupDetails = (props) => {
   return (
     <MeetupDetail
-      image="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
-      title="First Meetup"
-      address="Some street 5, Some City"
-      description="This is the first meetup"
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
+};
+
+export const getStaticPaths = async () => {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const meetupId = context.params.meetupId;
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
+        title: "First Meetup",
+        address: "Some street 5, Some City",
+        description: "This is the first meetup",
+      },
+    },
+  };
 };
 
 export default MeetupDetails;
